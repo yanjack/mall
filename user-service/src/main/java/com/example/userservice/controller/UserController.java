7b4598971420dee5beb3b036404042837c1e7cdd
yanlang <646065323@qq.com>
2019-11-13 19:35:05 +0800

    集成redis

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.userservice.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.example.mallcache.RedisUtil;
 import com.example.user.api.mode.UserBean;
 import com.example.userservice.service.UserService;
 
@@ -10,6 +11,7 @@ import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 //@Controller(value = "/api/user")
 @RestController
@@ -18,6 +20,9 @@ public class UserController  {
 
     @Resource
     UserService userService;
+
+    @Resource
+    RedisUtil redisUtil;
 
     @PostMapping(value = "/insert")
     public UserBean insert(@RequestBody UserBean userBean) {
@@ -33,5 +38,19 @@ public class UserController  {
     public PageInfo<UserBean> listAll(@RequestParam(value = "pageNo",required = false,defaultValue = "0") Integer pageNo,
                                       @RequestParam (value = "pageSize",required = false,defaultValue = "3") Integer pageSize) {
         return userService.listAll(pageNo,pageSize);
+    }
+
+    @GetMapping(value = "redis")
+    public String redisSet(){
+        redisUtil.set("redis","hello");
+        return redisUtil.get("redis").toString();
+    }
+    @GetMapping(value = "redis2")
+    public UserBean redisSet2(){
+        UserBean userBean = new UserBean();
+        userBean.setCtime(new Date());
+        userBean.setId(111);
+        redisUtil.set("redis2",userBean);
+        return (UserBean) redisUtil.get("redis2");
     }
 }
