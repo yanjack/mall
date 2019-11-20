f9646ba725a51c228627f05c0524accc3d6c364f
yanlang <646065323@qq.com>
2019-11-20 19:54:57 +0800

    刷新白名单

--- mall-gateway/src/main/java/com/example/mallgateway/controller/AdminController.java
+++ mall-gateway/src/main/java/com/example/mallgateway/controller/AdminController.java
@@ -1,10 +1,30 @@
 package com.example.mallgateway.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.mallgateway.config.URLWhritList;
+import com.example.mallgateway.database.GatewayInterfaceBean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.annotation.Resource;
+import java.util.Map;
+
+@RestController
 @RequestMapping("admin")
 public class AdminController {
+
+    @Resource
+    URLWhritList urlWhritList;
+
+    @GetMapping("reload")
+    public String reload(){
+        urlWhritList.reload();
+        return "ok";
+    }
+
+    @GetMapping("list")
+    public Map<String, GatewayInterfaceBean> list(){
+        return URLWhritList.getMaps();
+    }
 
 }
